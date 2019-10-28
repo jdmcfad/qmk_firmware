66206e81fd4cb439560cfd934275af32e386c4e5
Josh <josh@visionistinc.com>
2019-10-28 14:24:20 -0400

    Turning off the Planck number row safety blanket. Changing thumb Del to Enter.

--- keyboards/rgbkb/zen/rev2/keymaps/mindsound/keymap.c
+++ keyboards/rgbkb/zen/rev2/keymaps/mindsound/keymap.c
@@ -35,27 +35,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------..------+------+------+------+------+------+------|
    * |Shift |   Z  |   X  |   C  |   V  |   B  |RGBPrv||RGBNxt|   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+------||------+------+------+------+------+------+------|
-   * |Media | Del  | GUI  | Alt  |Raise |HSpace| Del  || Bksp |Space |Lower | Left | Down | Up   |Right |
+   * |Media | Del  | GUI  | Alt  |Raise |HSpace|Enter || Bksp |Space |Lower | Left | Down | Up   |Right |
    * `----------------------------------+-------------''------------------------------------------------'
-   *                                    |HSpace| Del  || Bksp |Space |
+   *                                    |HSpace|Enter || Bksp |Space |
    *                                    '-------------''-------------'
    */
   [_QWERTY] = LAYOUT( \
     KC_GESC,   KC_1,    KC_2,    KC_3,    KC_4,  KC_5,                       KC_6,     KC_7,  KC_8,    KC_9,    KC_0,    KC_BSPC, \
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,                       KC_Y,     KC_U,  KC_I,    KC_O,    KC_P,    KC_BSLS, \
     CTRL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,  KC_G,                       KC_H,     KC_J,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    RGB_RMOD,RGB_MOD,  KC_N,     KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-    KC_MPLY,   KC_DEL,  KC_LGUI, KC_LALT, LOWER, HYPR_SPC,KC_DEL, KC_BSPC,  KC_SPACE, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,\
-                                                 HYPR_SPC,KC_DEL, KC_BSPC,  KC_SPACE \
+    KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,   KC_B,      KC_N,     KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
+    KC_MPLY,   KC_DEL,  KC_LGUI, KC_LALT, LOWER, HYPR_SPC,KC_ENT, KC_BSPC,   KC_SPACE, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,\
+                                                 HYPR_SPC,KC_ENT, KC_BSPC,   KC_SPACE \
   ),
 
   /* LOWER
    */
   [_LOWER] = LAYOUT( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
+    KC_TILD, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
     _______, RGB_SAI, RGB_VAI, RGB_SAD, _______, _______,                   _______, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
-    _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_END,  _______, \
+    _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, RGB_RMOD,RGB_MOD, _______, _______, _______, KC_HOME, KC_END,  _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, \
                                                  _______, _______, _______, _______ \
   ),
@@ -64,9 +64,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [_RAISE] = LAYOUT( \
     KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, \
+    KC_GRV,  _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, \
     _______, RGB_SAI, RGB_VAI, RGB_SAD, _______, _______,                   _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
-    _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_PGDN, _______, \
+    _______, RGB_HUD, RGB_VAD, RGB_HUI, _______, _______, RGB_RMOD,RGB_MOD, _______, _______, _______, KC_PGUP, KC_PGDN, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY, \
                                                  _______, _______, _______, _______ \
   ),
